880fbdc23f34664afefc60686ef742e08fcf5655
elliottower <32176771+elliottower@users.noreply.github.com>
2018-08-07 12:10:30 +0200

    added interface

--- experiment.py
+++ experiment.py
@@ -5,3 +5,21 @@ def run(board_size, start_row, start_col, genration_max):
     best_path_found = [(2,2), (1,4), (3,3), (4,1)]
 
     return best_path_found
+
+class IndividualGene:
+    def __init__(self, configuration):
+        self.configuration = configuration
+
+    def mutate(self):
+        pass
+
+    def crossover(self, other):
+        pass
+
+
+class Configuration:
+    def __init__(self, board_size, start_row, start_col, generation_max):
+        self.board_size = board_size
+        self.start_row = start_row
+        self.start_col = start_col
+        self.generation_max = generation_max
