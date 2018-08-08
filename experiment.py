94d23a0d60157782ddc6ca8908b0ec85abade4d8
Elliot Tower <etower@umass.edu>
2018-08-08 12:02:08 +0200

    Added in line comments & documentation

--- experiment.py
+++ experiment.py
@@ -1,23 +1,35 @@
 import random
 import copy
 import interface
-
+# The survival ratio determines the proportion of the population that survives.
+# So it signifies our willingness to explore the potential of currently bad solutions. If this proportion is high
+# we have a higher genetic diversity, but also less exploration per generation, so a slower development.
 survivalRatio = 0.5
+
+# Two types of reproduction are implemented: mutation and cross-over,
+# if reproductionTypeRatio is 1, only matuation is used,
+# if it is 0, only cross-over is used. Otherwise a proportion of both.
 reproductionTypeRatio = 0.5
+
+# If elite is true then only the best solutions survive, instead of also some random ones.
 elite = False
-#nrOfGenerations = 99
+
+# It prints the best solution of every generation
 printValues = True
 
 def generation(population):
-
+    """generates a population's offspring"""
     # Sort the solutions by merit (they've already calculated their own fitness upon being created)
     population.sort(key=lambda x: x.fitness(), reverse=True)
 
     # Determine the number of solutions allowed to survive from the given survival ratio
     # and how much room that leaves for children.
     survivors = int(len(population) * survivalRatio)
+
+    # number of children that need to be born to restore the same population size
     nr_of_children = len(population) - survivors
 
+    # number of asexually born children through mutation
     nr_of_cloned_children = int(nr_of_children * reproductionTypeRatio)
 
     # If the elite variable is set to false, we want to keep some solutions that don't look promising
@@ -45,14 +57,14 @@ def generation(population):
     return population
 
 def make_population(conf, population_size):
-    """makes a population"""
+    """makes a population by starting with an empty list and then repeatedly appends individual genes into this list"""
     population = []
     for i in range(population_size):
         population.append(interface.IndividualGene(conf))
     return population
 
 def findbest(population):
-    """finds best individual from a population"""
+    """finds best individual from a population, the higher the fitness the better"""
     best = population[0]
     for i in population:
         if i.fitness() > best.fitness():
@@ -61,6 +73,7 @@ def findbest(population):
 
 
 def reproduce(individual, partner=None):
+    """checks if the individual is supposed to reproduce asexually by mutation or sexually by crossover"""
     if partner is None:
         child = copy.deepcopy(individual)
         child.mutate()
@@ -81,11 +94,20 @@ def run(board_size, start_row, start_col, evaluation_max):
     # importing the configuration
     conf = interface.Configuration(board_size, start_row, start_col, evaluation_max)
     individual = interface.IndividualGene(conf)
+
+    """makes the population size the square of the board size, so on an 8x8 chess board the population is 64"""
     population_size = conf.board_size ** 2
+
+    """makes a population"""
     population = make_population(conf, population_size)
-    nrOfGenerations = int((evaluation_max-population_size*(survivalRatio))/
+
+    """calculates the total number of generations based off the original parents, plus the amount of children per 
+    generation, times the amount of generations """
+    nrOfGenerations = int((evaluation_max - (population_size*survivalRatio))/
                           (population_size*(1-survivalRatio)))
 
+    """"loops through and generates the population 
+    repeats it nrOfGenerations - 1 times because you already start with one generation"""
     for i in range(nrOfGenerations-1):
         population = generation(population)
 
@@ -94,8 +116,7 @@ def run(board_size, start_row, start_col, evaluation_max):
 
             print("Best solution in population " + str(i + 2) + ": length: "+ str(best.fitness()) + ", solution: " + str(best.path()))
 
-    # best = findBest(population)
-    # [(2,2), (1,4), (3,3), (4,1)]
+    """finds the longest route in the population, returns the path"""
     best_path_found = findbest(population).path()
 
     return best_path_found
